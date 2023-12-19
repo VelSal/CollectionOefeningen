e43bf3bcc7ae2bea5e0a9340fd2de1aa9f61efdc
VelSal <v.salvatore@live.be>
2023-12-19 10:43:41 +0100

    Added exercice 1

---
+++ Oefening1/Models/WordsList.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Oefening1.Models
+{
+    public class WordsList
+    {
+        public static void AskWord()
+        {
+            bool isRunning = true;
+            List<string> words = new List<string>();
+
+            while (isRunning)
+            {
+                Console.Write("Give me a word: ");
+                string wordUserInput = Console.ReadLine().ToLower();
+
+                if (!words.Contains(wordUserInput) && wordUserInput != "end")
+                {
+                    Console.WriteLine($"{WordsList.GreenAdded()}");
+                    Console.ResetColor();
+                    words.Add(wordUserInput);
+                }
+                else if (words.Contains(wordUserInput) && wordUserInput != "end")
+                {
+                    Console.WriteLine($"{WordsList.RedNotAdded()}");
+                    Console.ResetColor();
+                }
+                else
+                {
+                    Console.Clear();
+                    Console.Write("Ending Program.\nThe words in your list are: ");
+                    string listString = string.Join(", ", words);
+                    foreach (var item in listString)
+                    {
+                        Console.ForegroundColor = ConsoleColor.Blue;
+                        Console.Write(item);
+                        Console.ResetColor();
+                    }
+                    isRunning = false;
+                }
+            }
+        }
+        static string GreenAdded()
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            return "Added";
+        }
+        static string RedNotAdded()
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            return "Not Added";
+        }
+    }
+}
---
+++ Oefening1/Program.cs
@@ -0,0 +1,18 @@
+﻿using Oefening1.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Oefening1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            WordsList.AskWord();
+            Console.ReadLine();
+        }
+    }
+}
