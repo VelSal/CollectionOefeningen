92951028436a80fee68c05ddd86538ae73945ac0
VelSal <v.salvatore@live.be>
2023-12-19 11:59:43 +0100

    suite ex2

--- Oefening2/Models/Dealer.cs
+++ Oefening2/Models/Dealer.cs
@@ -8,5 +8,17 @@ namespace Oefening2.Models
 {
     public class Dealer
     {
+        private string _address = "Rouppeplein 16, 1000 Brussel";
+        private double _moneyInAccount = 473491.0;
+        public int carsAvailable;
+
+        public string Address 
+        {
+            get { return _address; }
+        }
+        public double MoneyInAccount
+        { 
+            get { return _moneyInAccount; }
+        }
     }
 }
