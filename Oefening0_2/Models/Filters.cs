43d850c81a0291d921ebe277d0231fb2c990b659
VelSal <v.salvatore@live.be>
2023-12-21 09:27:59 +0100

    Exercice 0_2

---
+++ Oefening0_2/Models/Filters.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Oefening0_2.Models
+{
+    public static class Filters
+    {
+        public static void Filters10(int begin, int end)
+        {
+            List<int> ints = new List<int>();
+            for (int i = begin; i <= end; i++)
+            {
+                if (i % 10 == 0)
+                {
+                    ints.Add(i);
+                }
+            }
+
+            foreach (int i in ints)
+            {
+                Console.WriteLine(i);
+            }
+        }
+        public static void Filters3And5(int begin, int end)
+        {
+            List<int> ints = new List<int>();
+            for (int i = begin; i <= end; i++)
+            {
+                if (i % 3 == 0 && i % 5 == 0)
+                {
+                    ints.Add(i);
+                }
+            }
+
+            foreach (int i in ints)
+            {
+                Console.WriteLine(i);
+            }
+        }
+
+        
+    }
+}
--- Oefening0_2/Program.cs
+++ Oefening0_2/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Oefening0_2.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +11,13 @@ namespace Oefening0_2
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("=== LOOP NUMBERS ===");
+            Console.WriteLine("\nStarting loop 1 with filtering.\nOnly print numbers divisible by 10:");
+            Filters.Filters10(50, 150);
+            Console.WriteLine("\nStarting loop 2 with filtering.\nOnly print numbers divisible by 3 & 5:");
+            Filters.Filters3And5(50, 150);
+
+            Console.ReadLine();
         }
     }
 }
